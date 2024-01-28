4ceb9524f4697b43be4929003c483dc75bf1b17f
Amity Z <38151042+amityz@users.noreply.github.com>
2024-01-28 16:07:47 -0600

    Background Reformatting
    new files and CSS changes that should have no visual impact.

---
+++ fixlist/modal.js
@@ -0,0 +1,10 @@
+class Job{
+    constructor(title, subtitle, body){
+    this.title = title;
+    this.subtitle = subtitle;
+    this.body = body;
+    }
+}
+
+const jobs = [];
+jobs.push(new Job("Looking to Chrome Up", "Little China, Service, Indeterminate Pay - Fixer", "I have some Tyger Claw clients looking to get some stylish new chrome and weapons, whatever you can find. Will pay based on what you can bring, scavved is fine. Must be working or fixed on-site, with a hefty bonus if you can install it when you bring it. Feel free to reach out, call if you want to meet. Or you can drop me a line at the Forlorn Hope...or Silver Dragon, if you\'re willing to buy me a drink."));
